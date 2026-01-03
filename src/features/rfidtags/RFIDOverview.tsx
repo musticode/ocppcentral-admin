a19d539969267d0739bc08d80a123dd630bd1a1d
mustafakaratas <mustafakaratas4179@gmail.com>
2026-01-04 02:52:43 +0300

    feat: rfid cart create modal added

--- src/features/rfidtags/RFIDOverview.tsx
+++ src/features/rfidtags/RFIDOverview.tsx
@@ -1,13 +1,33 @@
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 import RFIDTagsList from "./RFIDTagsList";
+import { Button } from "@/components/ui/button";
+import { Plus } from "lucide-react";
+import { useState } from "react";
+import { CreateRFIDTagModal } from "./CreateRFIDTagModal";
 
 export const RFIDOverview = () => {
+  const [createRFIDTagModalOpen, setCreateRFIDTagModalOpen] = useState(false);
   return (
     <div className="p-6">
       <Card>
         <CardHeader>
           <CardTitle>RFID Tags Overview</CardTitle>
         </CardHeader>
+        <CardContent>
+          <Button onClick={() => setCreateRFIDTagModalOpen(true)}>
+            <Plus className="mr-2 h-4 w-4" />
+            Add RFID Tag
+          </Button>
+        </CardContent>
+        <CreateRFIDTagModal
+          open={createRFIDTagModalOpen}
+          onOpenChange={setCreateRFIDTagModalOpen}
+          onSuccess={() => {
+            // Refresh RFID tag list
+            console.log("RFID Tag created!");
+            setCreateRFIDTagModalOpen(false);
+          }}
+        />
         <CardContent>
           <RFIDTagsList />
         </CardContent>
